b94caa06447fd0ce69a87d734624e96d4391907e
My Name <samat.ibragimov.2000@bk.ru>
2025-05-24 02:28:14 +0300

    fffff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
@@ -9,12 +9,13 @@ import Cart from './pages/Cart';
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/product/:id" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/stickers-shop" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
